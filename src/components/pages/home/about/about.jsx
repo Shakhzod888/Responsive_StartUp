c94d3b071d035b1f47aa6629801c681f160a5b6c
Shakhzod <ShakhzodTurabidinov@gmail.com>
2023-09-14 17:03:33 +0500

    added home_section_2 page with responsive

---
+++ src/components/pages/home/about/about.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../about/about.scss";
+
+function About() {
+  return <section className="home_section_3">About</section>;
+}
+
+export default About;
--- src/components/pages/home/pagesMain.jsx
+++ src/components/pages/home/pagesMain.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Main from "./main/main";
+import About from "./about/about";
+import Sponsors from "./sponsors/sponsors";
 
 function PagesMain() {
   return (
     <div>
       <Main />
+      <Sponsors />
+      <About />
     </div>
   );
 }
